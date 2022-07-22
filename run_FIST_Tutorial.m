f328b9b482f2b5270eb57563017ce474633ee8d7
kuanglab <kuan0009@umn.edu>
2022-07-22 15:50:40 -0500

    Update run_FIST_Tutorial.m
    The first working version.

--- run_FIST_Tutorial.m
+++ run_FIST_Tutorial.m
@@ -3,17 +3,17 @@
 % W: PPI and spatial graph
 
 work_path = 'yourpath/FIST_data';%replace yourpath with your working path
-data_path = 'FIST_data/FIST_tutorial_output/';
-utils_path = 'FIST_data/FIST_utils';
+data_path = 'FIST_Tutorial_Output/';
+utils_path = 'FIST_utils';
 data_name = 'V1_Human_Heart';
-PPI_name = 'HSA_PPI.mat'; %use HSA_PPI.mat for human data and MUS_PPI.mat for mouse data
+PPI_name = 'HSA_PPI'; %use HSA_PPI.mat for human data and MUS_PPI.mat for mouse data
 
 cd(work_path);
 [V,W,rank_k] = data_prep_10x(data_name, PPI_name, data_path, utils_path);
 
 %set parameters
 opts.stopcrit = 10^-4;
-opts.MaxIters = 500;
+opts.MaxIters = 5;
 opts.dense_mod = 0; % use dense implementation of FIST if the data is NOT sparse
 opts.rank = rank_k;
 opts.lambda = 0.01; %parameter for visium data
@@ -26,7 +26,8 @@ A = FIST(V,M,W,opts);
 
 % get the output tensor
 Y = tensor(ktensor(A)); % output tensor in cpd-form
-disp(Y)
+
 
 % save both the factor matrices and the tensor
-save([data_path,'/',data_name,'_output.mat'],'A','Y');
+cd(work_path);
+save([data_path,data_name,'_output.mat'],'A','Y');
